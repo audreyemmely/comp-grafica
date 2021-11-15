20a1490f910abf418b76e75cabe5fa39969dec9e
Audrey <aerv@ic.ufal.br>
2021-11-15 10:55:42 -0300

    testando bibliotecas

---
+++ aulas-monitoria/test.c
@@ -0,0 +1,24 @@
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+void display(){}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow("OpenGL");
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
+    return 0;
+
+}
+/// rodou com comando diferente do q foi mostrado pelo monitor, nao sei pq deu erro, mas achei outro jeito de rodar 
+/// gcc test.c -o test.out -lglut -lGL -lGLU
+/// ./test.out
