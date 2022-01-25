9d749b03d8b1f334f1a575a558e281c554e8e06a
Audrey <aerv@ic.ufal.br>
2022-01-25 10:17:39 -0300

    Update sistemaIluminado.c

--- projetos/sistemaIluminado.c
+++ projetos/sistemaIluminado.c
@@ -3,12 +3,13 @@
 GLsizei winWidth = 1000, winHeight = 700;
 
 static int mercurio, venus, terra, marte, ceres, luas = 0;
- 
+
+// Inicializa parâmetros de rendering
 void init(void)
 {
-    glClearColor(0, 0, 0, 0);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
+    	glClearColor(0, 0, 0, 0); // Define a cor de fundo da janela de visualização como preta
+	glEnable(GL_DEPTH_TEST); // Habilita o depth-buffering
+	glEnable(GL_LIGHTING); 	// Habilita o uso de iluminação
 	glEnable(GL_COLOR_MATERIAL);
 	glEnable(GL_LIGHT0);
 	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0);
@@ -18,98 +19,98 @@ void init(void)
  
 void display(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT |  GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT |  GL_DEPTH_BUFFER_BIT); // Limpa a janela e o depth buffer
 
 //  Sol
-    glColor3f(1, 1, 0); //Cor amarela
+    glColor3f(1, 1, 0); // Cor amarela
      
     glPushMatrix();
-    glRotatef((GLfloat) 90, 1, 0, 0); //Rotacionar o sol
-    glRotatef((GLfloat) terra, 0, 0, 1); //Rotacionar o sol de acordo com o movimento dos planetas
-    glutSolidSphere(1, 20, 16); //Desenha o sol
+    glRotatef((GLfloat) 90, 1, 0, 0); // Rotacionar o sol
+    glRotatef((GLfloat) terra, 0, 0, 1); // Rotacionar o sol de acordo com o movimento dos planetas
+    glutSolidSphere(1, 20, 16); // Desenha o sol
     glPopMatrix();
 
 //  Mercurio (planeta 1)
     glColor3f(0.0, 1.0, 1.0); //Cor ciano
      
     glPushMatrix();
-    glRotatef((GLfloat) mercurio, 0, 1, 0.15); //Rotacionar planeta mercurio ao redor do sol
-    glTranslatef(1.2, 0, 0); //Transladar planeta mais perto do sol
-    glutSolidSphere(0.05, 12, 10); //Desenha planeta
+    glRotatef((GLfloat) mercurio, 0, 1, 0.15); // Rotacionar planeta mercurio ao redor do sol
+    glTranslatef(1.2, 0, 0); // Transladar planeta mais perto do sol
+    glutSolidSphere(0.05, 12, 10); // Desenha planeta
     glPopMatrix();
 
 //  Venus (planeta 2)
-    glColor3f(1.0, 0.5, 0.0); //Cor laranja
+    glColor3f(1.0, 0.5, 0.0); // Cor laranja
      
     glPushMatrix();
-    glRotatef((GLfloat) venus, 0, 1, 0.1); //Rotacionar planeta venus ao redor do sol
-    glTranslatef(1.5, -0.3, 0); //Transladar planeta entre mercurio e terra
-    glutSolidSphere(0.07, 12, 10); //Desenha planeta
+    glRotatef((GLfloat) venus, 0, 1, 0.1); // Rotacionar planeta venus ao redor do sol
+    glTranslatef(1.5, -0.3, 0); // Transladar planeta entre mercurio e terra
+    glutSolidSphere(0.07, 12, 10); // Desenha planeta
     glPopMatrix();
 
 //  Terra (planeta 3)
-    glColor3f(0, 0, 1); //Cor azul
+    glColor3f(0, 0, 1); // Cor azul
      
     glPushMatrix();
-    glRotatef((GLfloat) (terra/2), 0, 1, 0); //Rotacionar a terra mais devagar ao redor do sol
-    glTranslatef(2, 0, 0); //Transladar a terra para que ela possa rotacionar ao redor do sol
-    glutSolidSphere(0.2, 10, 8); //Desenha a terra
+    glRotatef((GLfloat) (terra/2), 0, 1, 0); // Rotacionar a terra mais devagar ao redor do sol
+    glTranslatef(2, 0, 0); // Transladar a terra para que ela possa rotacionar ao redor do sol
+    glutSolidSphere(0.2, 10, 8); // Desenha a terra
     glPopMatrix();
 
 //  Primeira lua
     glColor3f(1, 1, 1); //Cor branca
      
     glPushMatrix();
-    glRotatef((GLfloat) (terra/2), 0, 1, 0); //Rotacionar a lua para que ela gire junto com a Terra
-    glTranslatef(2, 0, 0); //Transladar a lua para que ela fique junto com a Terra
-    glRotatef((GLfloat) luas, 0, 1, 0); //Rotacionar a lua para que ela gire ao redor do eixo X da Terra
-    glTranslatef(0.5, 0, 0); //Transladar a lua para ficar a 0.5 de distância da Terra
-    glutSolidSphere(0.05, 8, 6); //Desenha a lua
+    glRotatef((GLfloat) (terra/2), 0, 1, 0); // Rotacionar a lua para que ela gire junto com a Terra
+    glTranslatef(2, 0, 0); // Transladar a lua para que ela fique junto com a Terra
+    glRotatef((GLfloat) luas, 0, 1, 0); // Rotacionar a lua para que ela gire ao redor do eixo X da Terra
+    glTranslatef(0.5, 0, 0); // Transladar a lua para ficar a 0.5 de distância da Terra
+    glutSolidSphere(0.05, 8, 6); // Desenha a lua
     glPopMatrix();
 
 //  Segunda lua
-    glColor3f(0, 1, 0); //Cor verde
+    glColor3f(0, 1, 0); // Cor verde
      
     glPushMatrix();
-    glRotatef((GLfloat) (terra/2), 0, 1, 0); //Rotacionar a lua para que ela gire junto com o planeta
-    glTranslatef(2, 0, 0); //Transladar a lua para que ela fique junto com o planeta
-    glRotatef((GLfloat) luas, 1, 1, 0); //Rotacionar a lua para que ela gire ao redor do eixo x e y da Terra
-    glTranslatef(0, 0, 0.5); //Transladar a lua para ficar a 0.5 de distância do centro do planeta
-    glutSolidSphere(0.05, 8, 6); //Desenha a lua
+    glRotatef((GLfloat) (terra/2), 0, 1, 0); // Rotacionar a lua para que ela gire junto com o planeta
+    glTranslatef(2, 0, 0); // Transladar a lua para que ela fique junto com o planeta
+    glRotatef((GLfloat) luas, 1, 1, 0); // Rotacionar a lua para que ela gire ao redor do eixo x e y da Terra
+    glTranslatef(0, 0, 0.5); // Transladar a lua para ficar a 0.5 de distância do centro do planeta
+    glutSolidSphere(0.05, 8, 6); // Desenha a lua
     glPopMatrix();
 
 //  Marte (planeta 4)
-    glColor3f(1, 0, 0); //Cor vermelha
+    glColor3f(1, 0, 0); // Cor vermelha
      
     glPushMatrix();
-    glRotatef((GLfloat) (marte/3), 0, 1, 0); //Rotacionar planeta marte ao redor do sol
-    glTranslatef(-2.5, 0.4, 0); //Transladar planeta para o outro lado do sol entre venus e ceres
-    glutSolidSphere(0.1, 10, 8); //Desenha o planeta
+    glRotatef((GLfloat) (marte/3), 0, 1, 0); // Rotacionar planeta marte ao redor do sol
+    glTranslatef(-2.5, 0.4, 0); // Transladar planeta para o outro lado do sol entre venus e ceres
+    glutSolidSphere(0.1, 10, 8); // Desenha o planeta
     glPopMatrix();
 
 // Ceres (planeta-anão 5)
-    glColor3f(1.0, 0.0, 1.0); //Cor rosa
+    glColor3f(1.0, 0.0, 1.0); // Cor rosa
      
     glPushMatrix();
-    glRotatef((GLfloat) (ceres/8), 0, 1, 0.35); //Rotacionar planeta ceres ao redor do sol
-    glTranslatef(-3, 0, 0); //Transladar planeta mais longe do sol
-    glutSolidSphere(0.05, 5, 5); //Desenha planeta
+    glRotatef((GLfloat) (ceres/8), 0, 1, 0.35); // Rotacionar planeta ceres ao redor do sol
+    glTranslatef(-3, 0, 0); // Transladar planeta mais longe do sol
+    glutSolidSphere(0.05, 5, 5); // Desenha planeta
     glPopMatrix();
 
-    glutSwapBuffers();
+    glutSwapBuffers(); // Realiza troca de buffers
 }
 
 
 void reshape(GLint w, GLint h)
 {
-    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h); // Especifica o tamanho da viewport
      
-    glMatrixMode(GL_PROJECTION); 
-    gluPerspective(60, (GLfloat) w/(GLfloat) h, 1, 20);
-    
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+    glMatrixMode(GL_PROJECTION);  // Especifica sistema de coordenadas de projeção
+    gluPerspective(60, (GLfloat) w/(GLfloat) h, 1, 20); // Especifica a projeção perspectiva
+
+    glMatrixMode(GL_MODELVIEW);	// Especifica sistema de coordenadas do modelo
+    glLoadIdentity(); // Inicializa sistema de coordenadas do modelo
+    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0); // Especifica posição do observador e do alvo
 }
  
 void keyboard(unsigned char key, int x, int y)
@@ -139,19 +140,22 @@ void keyboard(unsigned char key, int x, int y)
 int main(int argc, char *argv[])
 {
      
-    glutInit(&argc, argv);
+    glutInit(&argc, argv); 
      
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(winWidth, winHeight);
+    glutInitWindowSize(winWidth, winHeight); // Especifica o tamanho em pixels da janela GLUT
     glutCreateWindow("Sistema Solar Iluminado");
      
-    init();
+    init(); // Nesta função são feitas as inicializações OpenGL que devem ser executadas antes do rendering
+     		
+    glutDisplayFunc(display); // Estabelece a função "display" previamente definida como a função callback de exibição, 
+			      //isto significa que a GLUT chama a função sempre que a janela precisar ser redesenhada
+    glutReshapeFunc(reshape); // Sempre que a janela é maximizada, minimizada, etc., a função "reshape" é executada para reinicializar o sistema de coordenadas
+    glutKeyboardFunc(keyboard); // Estabelece a função callback que é chamada pela GLUT cada vez que uma tecla que gera código ASCII é pressionada. 
+				//Além do valor ASCII da tecla, a posição (x,y) do mouse quando a tecla foi pressionada também é retornada.
      
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
-     
-    glutMainLoop();
+    glutMainLoop(); // Inicia a execução da “máquina de estados” e processa todas as mensagens específicas do sistema operacional, 
+		    //como teclas e botões do mouse pressionados, até que o programa termine
      
     return 0;
 }
